5a1dfb1a0a6f14e0a3cbb38a7a6abcd50869b7e9
he1067205831 <1067205831@qq.com>
2016-10-08 16:58:29 +0800

    提交本地

---
+++ src/Cal.java
@@ -0,0 +1,34 @@
+//四则运算类
+public class Cal {
+	//属性：2个运算数
+	int a;
+	int b;
+	int c;
+	int d;
+	//行为
+	
+	//加法
+	
+	int jia(){
+		//实现a + b
+		c=a+b;
+		return c;	
+	}
+	//减法
+	int jian(){
+		c=a-b;
+		return c;
+	}
+	//乘法
+	int cheng(){
+		c=a*b;
+		return c;
+	}
+	//除法
+	int chu(){
+		c=a/b;
+		return c ;
+	}
+	
+	
+}
---
+++ src/TestCal.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class TestCal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("请输入一个数字");
+		int  g=s.nextInt();
+		while(true){	
+			 
+		System.out.println("请输入一个运算方式，+,-,*,/");
+		String l=s.next();
+		System.out.println("请输入一个数字");
+		int  f=s.nextInt();	
+		
+		char ch=l.charAt(0);
+		//创建一个Cal的对象
+		
+		Cal cl = new Cal();
+		
+		//属性：赋值
+		cl.a = g;
+		cl.b = f;
+		
+		//行为：运算
+		if(ch=='+'){
+		int c=cl.jia();
+		g=c;
+		System.out.println("结果是"+c);
+		}else if(ch=='-'){
+		//...
+		 int c=cl.jian();
+		 g=c;
+		System.out.println("结果是"+c);
+		}else if(ch=='*'){
+		int  c=cl.cheng();
+		g=c;
+		System.out.println("结果是"+c);
+		}else if(ch=='/'){
+		int c=cl.chu();
+		g=c;
+		System.out.println("结果是"+c);
+		}else{
+			System.out.println("输入有误");
+		}
+		
+		}
+		
+	}
+
+}
